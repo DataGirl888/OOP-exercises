e32e0e4cea8a1cc442c40e1be6c5b9b1bdc7f7f8
Zainab <datadrivenproductivity@gmail.com>
2020-07-19 13:14:15 +0200

    Exercise 3, Author book classes in progress

--- src/main/java/se/lexicon/zainabahmed/books/Author.java
+++ src/main/java/se/lexicon/zainabahmed/books/Author.java
@@ -1,29 +1,139 @@
 package se.lexicon.zainabahmed.books;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Author {
+//OOP EXERCISE 3.
+//Each book has only one author
+//Each author can write multiple books
+
     private int authorID;
     private String foreName;
     private String surName;
     private String genre;
     private int booksWritten;
     private String [] publicationList;
+
     private static String [] authorsArray;
     private static int countAuthors;
 
     //constructors
+    //no arg constructor
+    public Author(){
+
+    }
+    //name genre constructor
+    public Author(String foreName, String surName, String genre){
+
+    }
+    //name constructor
+    public Author(String foreName, String surName){
+
+    }
+    //most parameters (not list of books written) constructor
+    public Author(String foreName, String surName, String genre, int booksWritten) {
+        this.foreName = foreName;
+        this.surName = surName;
+        this.genre = genre;
+        this.booksWritten = booksWritten;
+
+    }
+    //all parameters constructor, including list of publications
     public Author(String foreName, String surName, String genre, int booksWritten, String[] publicationList) {
         this.foreName = foreName;
         this.surName = surName;
         this.genre = genre;
         this.booksWritten = booksWritten;
-        this.publicationList = publicationList;
+        
     }
     //getters and setters
 
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getForeName() {
+        return foreName;
+    }
+
+    public void setForeName(String foreName) {
+        this.foreName = foreName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getBooksWritten() {
+        return booksWritten;
+    }
+
+    public void setBooksWritten(int booksWritten) {
+        this.booksWritten = booksWritten;
+    }
+
+    public String[] getPublicationList() {
+        return publicationList;
+    }
+
+    public void setPublicationList(String[] publicationList) {
+        this.publicationList = publicationList;
+    }
+
+    public static String[] getAuthorsArray() {
+        return authorsArray;
+    }
+
+    public static void setAuthorsArray(String[] authorsArray) {
+        Author.authorsArray = authorsArray;
+    }
+
+    public static int getCountAuthors() {
+        return countAuthors;
+    }
+
+    public static void setCountAuthors(int countAuthors) {
+        Author.countAuthors = countAuthors;
+    }
+
     //main
     public static void main(String[] args) {
-
+        // passed to constructor, but getter returns null? returns value when set with setter ??
+        Author enidBlyton = new Author("Enid", "Blyton", "children's fiction" );
+        Author sidneySheldon = new Author("Sidney", "Sheldon", "Fiction" );
+        Author jkRowling = new Author("Joanne","Rowling");
+        Author calNewPort = new Author("Cal","Newport","Productivity");
+        Author tonyRobbins = new Author("Tony","Robbins","Success");
+        Author danielLiang = new Author("Daniel","Liang","Java");
+        Author josephConrad = new Author("Joseph","Conrad");
+//        enidBlyton.setAuthorID(1);
+//        enidBlyton.setForeName("Enid");
+//        //enidBlyton.setSurName("Blyton");
+//      //  System.out.println(enidBlyton.toString());
+//        System.out.println(enidBlyton.foreName);
+//       // System.out.println(enidBlyton.surName);
+//        System.out.println(enidBlyton.getForeName());
+//        System.out.println(sidneySheldon.getSurName());
+//        System.out.println(sidneySheldon.surName);
     }
+
+
     //methods
 public static void booksByAuthor(){   // will return a list
 
--- src/main/java/se/lexicon/zainabahmed/books/Book.java
+++ src/main/java/se/lexicon/zainabahmed/books/Book.java
@@ -1,20 +1,142 @@
 package se.lexicon.zainabahmed.books;
 import java.util.Arrays;
 
+//OOP EXERCISE 3.
+//Each book has only one author
+//Each author can write multiple books
+
 public class Book {
+    private int bookID;
     private String bookTitle;
-    private String authorName;
+    private String authorFirstName;
+    private String authorSurName;
     private String publicationDate;
-    private String ISSBN;
+    private String ISBN;
     private String language;
+    private String genre;
+
+    private static int countBooks;
+    private static String[] booksArray;
+
+    //constructors
+    //no arg constructor
+    public Book () {
+
+    }
+    //all values constructor
+    public Book (String bookTitle, String authorFirstName, String AuthorSurName, String genre,
+                 String publicationDate, String ISBN, String Language) {
+
+    }
+    //title, author full name constructor
+    public Book (String bookTitle, String authorFirstName, String AuthorSurName) {
+
+    }
+    //title, author full name, genre constructor
+    public Book (String bookTitle, String authorFirstName, String AuthorSurName, String genre) {
+
+    }
+    //title, author last name constructor
+    public Book (String bookTitle, String AuthorSurName) {
+
+    }
+    //title constructor
+    public Book (String bookTitle) {
+
+    }
+    //getters and setters
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorSurName() {
+        return authorSurName;
+    }
+
+    public void setAuthorSurName(String authorSurName) {
+        this.authorSurName = authorSurName;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public static int getCountBooks() {
+        return countBooks;
+    }
+
+    public static void setCountBooks(int countBooks) {
+        Book.countBooks = countBooks;
+    }
+
+    public static String[] getBooksArray() {
+        return booksArray;
+    }
+
+    public static void setBooksArray(String[] booksArray) {
+        Book.booksArray = booksArray;
+    }
 
     //main
     public static void main(String[] args) {
-
+    Book heartOfDarkness = new Book("Heart of Darkness","Joseph","Conrad");
+    Book potterPhoenix = new Book("Harry Potter and the Order of the Phoenix", "Rowling");
+    Book thankYouJeaves = new Book("Thank you, Jeaves","P.G.","Wodehouse");
+    Book deepWork = new Book("Deep Work","Cal","Newport");
+    Book getItDone = new Book("Get It Done!","David","Allen","Productivity");
     }
-    //
+    //methods
 
-    public static void printAuthor(){
+    public static void findBookAuthor(){   //
 
     }
 }
